a5f8a3a3eb91514a0ac9a0a446f5870ed8a6702a
saiaunghlyanhtet <saiaunghlyanhtet2003@gmail.com>
2024-08-15 17:14:53 +0630

    fix: add config for get, put, delete

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,7 +23,6 @@ resource "aws_iam_role" "lambda_exec_role" {
       {
         Action = "sts:AssumeRole",
         Effect = "Allow",
-        Sid    = "",
         Principal = {
           Service = "lambda.amazonaws.com",
         },
@@ -57,17 +56,65 @@ resource "aws_api_gateway_resource" "items" {
   path_part   = "items"
 }
 
-resource "aws_api_gateway_method" "items_method" {
+resource "aws_api_gateway_method" "items_post" {
   rest_api_id   = aws_api_gateway_rest_api.crud_api.id
   resource_id   = aws_api_gateway_resource.items.id
-  http_method   = "ANY"
+  http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda_integration" {
+resource "aws_api_gateway_method" "items_get" {
+  rest_api_id   = aws_api_gateway_rest_api.crud_api.id
+  resource_id   = aws_api_gateway_resource.items.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "items_put" {
+  rest_api_id   = aws_api_gateway_rest_api.crud_api.id
+  resource_id   = aws_api_gateway_resource.items.id
+  http_method   = "PUT"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "items_delete" {
+  rest_api_id   = aws_api_gateway_rest_api.crud_api.id
+  resource_id   = aws_api_gateway_resource.items.id
+  http_method   = "DELETE"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration_post" {
   rest_api_id             = aws_api_gateway_rest_api.crud_api.id
   resource_id             = aws_api_gateway_resource.items.id
-  http_method             = aws_api_gateway_method.items_method.http_method
+  http_method             = aws_api_gateway_method.items_post.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.crud_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "lambda_integration_get" {
+  rest_api_id             = aws_api_gateway_rest_api.crud_api.id
+  resource_id             = aws_api_gateway_resource.items.id
+  http_method             = aws_api_gateway_method.items_get.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.crud_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "lambda_integration_put" {
+  rest_api_id             = aws_api_gateway_rest_api.crud_api.id
+  resource_id             = aws_api_gateway_resource.items.id
+  http_method             = aws_api_gateway_method.items_put.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.crud_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "lambda_integration_delete" {
+  rest_api_id             = aws_api_gateway_rest_api.crud_api.id
+  resource_id             = aws_api_gateway_resource.items.id
+  http_method             = aws_api_gateway_method.items_delete.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.crud_lambda.invoke_arn
@@ -83,7 +130,10 @@ resource "aws_lambda_permission" "api_gateway_permission" {
 
 resource "aws_api_gateway_deployment" "api_deployment" {
   depends_on = [
-    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_integration.lambda_integration_post,
+    aws_api_gateway_integration.lambda_integration_get,
+    aws_api_gateway_integration.lambda_integration_put,
+    aws_api_gateway_integration.lambda_integration_delete,
   ]
 
   rest_api_id = aws_api_gateway_rest_api.crud_api.id
