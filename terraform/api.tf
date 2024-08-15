6bf2255a0ad88a5ee4301bc8a2962ab9b936e448
saiaunghlyanhtet <saiaunghlyanhtet2003@gmail.com>
2024-08-15 13:46:07 +0630

    fix: change handler name in api.tf

--- terraform/api.tf
+++ terraform/api.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "crud_lambda" {
   filename         = "lambda_function.zip"
   function_name    = "crudLambdaFunction"
   role             = aws_iam_role.lambda_exec_role.arn
-  handler          = "lambda_function.lambda_handler"
+  handler          = "api.lambda_handler"
   runtime          = "python3.9"
   source_code_hash = filebase64sha256("lambda_function.zip")
 }
