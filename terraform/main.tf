07b5cbf226943b2d9ab230a469880e340f59b798
saiaunghlyanhtet <saiaunghlyanhtet2003@gmail.com>
2024-08-15 15:51:21 +0630

    fix: change main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,89 @@
-module "provider" {
-  source = "./provider"
+provider "aws" {
+  region = "ap-northeast-1"
 }
 
-module "dynamodb" {
-  source = "./dynamodb"
+resource "aws_dynamodb_table" "items_table" {
+  name         = "items"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
 }
 
-module "api" {
-  source = "./api"
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Sid    = "",
+        Principal = {
+          Service = "lambda.amazonaws.com",
+        },
+      },
+    ],
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+    "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
+  ]
 }
 
-module "apigateway" {
-  source = "./apigateway"
+resource "aws_lambda_function" "crud_lambda" {
+  filename         = "lambda_function.zip"
+  function_name    = "crudLambdaFunction"
+  role             = aws_iam_role.lambda_exec_role.arn
+  handler          = "api.lambda_handler"
+  runtime          = "python3.9"
+  source_code_hash = filebase64sha256("lambda_function.zip")
 }
 
-module "iam" {
-  source = "./iam"
+resource "aws_api_gateway_rest_api" "crud_api" {
+  name        = "crud-api"
+  description = "CRUD API"
+}
+
+resource "aws_api_gateway_resource" "items" {
+  rest_api_id = aws_api_gateway_rest_api.crud_api.id
+  parent_id   = aws_api_gateway_rest_api.crud_api.root_resource_id
+  path_part   = "items"
+}
+
+resource "aws_api_gateway_method" "items_method" {
+  rest_api_id   = aws_api_gateway_rest_api.crud_api.id
+  resource_id   = aws_api_gateway_resource.items.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.crud_api.id
+  resource_id             = aws_api_gateway_resource.items.id
+  http_method             = aws_api_gateway_method.items_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.crud_lambda.invoke_arn
+}
+
+resource "aws_lambda_permission" "api_gateway_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.crud_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.crud_api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_deployment" "api_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.crud_api.id
+  stage_name  = "prod"
 }
